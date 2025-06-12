c70b56bbdb6ab46579d5462c4fc45b50dfd794a8
wanghongfei23 <whf00101@163.com>
2025-06-12 18:59:27 +0800

    Initial commit: C++ project config and debug notes

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    // 可在此处添加更多测试代码，方便后续调试演示
+    int num = 10;
+    num += 5;
+    cout << "num 的值为：" << num << endl;
+    return 0;
+}
